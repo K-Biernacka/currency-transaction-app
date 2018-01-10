9adfb4890cfb93021c96c2995ca340a62bd76351
Kinga Biernacka <kingabiernacka@op.pl>
2018-01-10 22:26:02 +0100

    totals added

--- app.js
+++ app.js
@@ -30,7 +30,7 @@
         let item = document.createElement('div');
         item.innerText = text;
         item.className = 'justify-content-between d-flex list-item';
-        let button = document.createElement('div');
+        let button = document.createElement('i');
         let amount = document.createElement('div');
         let exchange = document.createElement('div');
         exchange.className = 'amount-in-pln';
@@ -40,27 +40,44 @@
 
         exchange.innerText = `${amountInPln} PLN`;
 
-
-
-
         amountInEur = parseFloat(amountInEur).toFixed(2);
         amount.innerText = `${amountInEur} EUR`;
+        amount.className = 'amount-in-eur';
         button.addEventListener('click', function(event) {
             item.remove();
         });
 
-        button.className = 'btn';
+        button.className = 'fa fa-trash';
         button.setAttribute('type', 'submit');
 
         list.appendChild(item);
         item.appendChild(amount);
         item.appendChild(exchange);
         item.appendChild(button);
+        totalAmount();
     };
 
+    let totalAmount = () => {
+        let listEur = document.querySelectorAll('.amount-in-eur');
+        let sum = 0;
+        for (let i = 0; i < listEur.length; i++) {
+            let value = listEur[i].innerText;
+            let totalEur = parseFloat(value);
+            sum += totalEur;
+        }
+        let sumEur = document.getElementById('euro-sum');
+        sumEur.innerText = `${sum} Total EUR`;
 
-
-
+        // let listPln = document.querySelectorAll('.amount-in-pln');
+        // let plnSum = 0;
+        // for (let i = 0; i < listPln.length; i++) {
+        //     let pln = listPln[i].innerText;
+        //     let totalPln = parseFloat(pln);
+        //     plnSum += totalPln;
+        // }
+        // let sumPln = document.getElementById('plnSum');
+        // sumPln.innerText = `${plnSum} Total PLN`
+    };
 
 
 })();
